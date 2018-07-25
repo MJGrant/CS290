7e57c8c8694c6baaf7fa5b4eefeb77dd500d96e7
Mandi Grant <mandi018@gmail.com>
2018-07-25 16:08:44 -0500

    Added image carousel to main page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,3 @@
-var getCurrentMonth = function() {
-    console.log("getting month");
-    var dateObject = new Date();
-    var currentMonth = dateObject.getMonth();
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return monthNames[currentMonth];
-};
-
-var getCurrentYear = function() {
-    var dateObject = new Date();
-    var currentYear = dateObject.getYear();
-    return currentYear;
-};
+$('.carousel').carousel({
+  interval: 2000
+});

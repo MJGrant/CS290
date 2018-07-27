73e036999ca7ec764c5be3dfb149fed144bc7217
Mandi Grant <mandi018@gmail.com>
2018-07-27 14:00:14 -0500

    Fixed footer

--- server.js
+++ server.js
@@ -3,7 +3,13 @@
 
 var express = require('express');
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create(
+    {
+        defaultLayout:'main',
+        helpers: {
+        year: function() {return new Date().getFullYear();}
+    }
+});
 var bodyParser = require('body-parser');
 var path = require('path');
 
@@ -17,17 +23,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 
 app.get('/',function(request, response) {
-    var date = {};
-
-    //get the current month and year so homepage always looks current
-    var dateObject = new Date();
-    var currentMonth = dateObject.getMonth();
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    date.month = monthNames[currentMonth];
-
-    var currentYear = dateObject.getFullYear();
-    date.year = currentYear;
-    response.render('index', date);
+    response.render('index');
 });
 
 app.get('/about',function(request, response) {
@@ -39,11 +35,11 @@ app.get('/contact',function(request, response) {
 });
 
 app.get('/download',function(request, response) {
-    response.render('download');
+    response.render('download', pageData);
 });
 
 app.get('/privacy',function(request, response) {
-    response.render('privacy');
+    response.render('privacy', pageData);
 });
 
 //error response pages
